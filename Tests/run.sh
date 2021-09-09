3303622e3f594b045bf2e146a7f8abefb8c1b98b
mrfox99 <80600890+mrfox99@users.noreply.github.com>
2021-09-09 17:54:59 +0200

    Almost done

---
+++ Tests/run.sh
@@ -0,0 +1,1 @@
+python main.py --type all --authentication-address http://localhost:5000 --jwt-secret JWT_SECRET_KEY --roles-field roles --user-role election_official --administrator-role administrator --administrator-address http://localhost:5001 --station-address http://localhost:5002 --with-authentication
